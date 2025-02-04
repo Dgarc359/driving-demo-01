10dcd069973204afd151bfbf9ced3bf7e04be9b7
dgarc359 <87497958+Dgarc359@users.noreply.github.com>
2025-02-04 18:17:18 -0500

    initial impl for car driving

--- scripts/rigid_car.gd
+++ scripts/rigid_car.gd
@@ -1,62 +1,7 @@
 extends RigidBody3D
 class_name RigidCar
 
-@onready var wheel_br: WheelRayCast = $WheelBR
-@onready var wheel_fr: WheelRayCast = $WheelFR
-@onready var wheel_fl: WheelRayCast = $WheelFL
-@onready var wheel_bl: WheelRayCast = $WheelBL
-
-
-
-
-
-# EXAMPLE
-func look_follow(state: PhysicsDirectBodyState3D, current_transform: Transform3D, target_position: Vector3) -> void:
-	var forward_local_axis: Vector3 = Vector3(1, 0, 0)
-	var forward_dir: Vector3 = (current_transform.basis * forward_local_axis).normalized()
-	var target_dir: Vector3 = (target_position - current_transform.origin).normalized()
-	# this line doesnt work OOB
-	#var local_speed: float = clampf(speed, 0, acos(forward_dir.dot(target_dir)))
-	#if forward_dir.dot(target_dir) > 1e-4:
-		#state.angular_velocity = local_speed * forward_dir.cross(target_dir) / state.step
-	
-	pass
-
-
-func _ready():
-	
-	
-	#for wheel in wheels:
-		#if wheel:
-			#wheel.wheel_colliding.connect(float)
-	pass
-
-func float_vehicle(state: PhysicsDirectBodyState3D, current_transform: Transform3D):
-	print('floooatiitngngngngn')
-	var upward_local_axis: Vector3 = Vector3(0,1,0)
-	var upward_dir: Vector3 = (current_transform.basis * upward_local_axis).normalized()
-	#var target_dir: Vector3 = (upward_dir + current_transform.origin).normalized()
-	state.angular_velocity = 4 * upward_dir / state.step
-	pass
-
-
-
-
-func _integrate_forces(state:  PhysicsDirectBodyState3D):
-	var space_state = get_world_3d().direct_space_state
-	#pass
-	
-	#var target_position = $my_target_node3d_node.global_transform.origin
-	#look_follow(state, global_transform, target_position)
-	var wheels: Array[WheelRayCast] = [
-		wheel_br,
-		wheel_fr,
-		wheel_fl,
-		wheel_bl
-	]
-	
-	for wheel in wheels:
-		if wheel and wheel.is_colliding:
-			float_vehicle(state, global_transform)
-	
-	pass
+@export var suspension_rest_dist: float = 0.5
+@export var spring_strength: float = 25
+@export var spring_damper: float = 1
+@export var wheel_radius: float = 0.33
--- scripts/wheel_ray_cast.gd
+++ scripts/wheel_ray_cast.gd
@@ -3,25 +3,36 @@ class_name WheelRayCast
 
 signal wheel_colliding
 
+@onready var car: RigidCar = $".."
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+var previous_spring_length : float = 0.0
 
 func _physics_process(delta: float) -> void:
-	
-	
-	force_raycast_update()
-	
 	if is_colliding():
-		wheel_colliding.emit()
-		#print("collidingoregre")
+		# initial reference: https://www.youtube.com/watch?v=fe-8J7_WAq0
+		# NEEDS WORK
+		var susp_dir = global_basis.y
+		
+		var raycast_origin = global_position
+		var raycast_dest = get_collision_point()
+		var distance = raycast_dest.distance_to(raycast_origin)
+		
+		var contact = get_collision_point() - car.global_position
+		
+		var spring_length = clamp(distance - car.wheel_radius, 0, car.suspension_rest_dist)
+		
+		var spring_force = car.spring_strength * (car.suspension_rest_dist - spring_length)
+		
+		var spring_velocity = (previous_spring_length - spring_length) / delta
+		
+		var damper_force = car.spring_damper * spring_velocity
+		
+		var suspension_force = basis.y * (spring_force + damper_force)
+		
+		previous_spring_length = spring_length
+		
+		var point = Vector3(raycast_dest.x, raycast_dest.y + car.wheel_radius, raycast_dest.z)
+		
+		car.apply_force(susp_dir * suspension_force , point - car.global_position)
 	
 	pass
-
-
-## Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta: float) -> void:
-	#force_
-	#pass
