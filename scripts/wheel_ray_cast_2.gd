c7159d6f70b368d794bb7905714351f64f19d374
dgarc359 <87497958+Dgarc359@users.noreply.github.com>
2025-02-09 12:52:04 -0500

    remove useless methods in wheel ray cast

--- scripts/wheel_ray_cast_2.gd
+++ scripts/wheel_ray_cast_2.gd
@@ -98,13 +98,3 @@ func apply_spring_force(delta: float, vehicle: RigidCar, vehicle_rotation: Quate
 	# TODO
 	# wheel.transform.origin = wheel_position
 	pass
-
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
