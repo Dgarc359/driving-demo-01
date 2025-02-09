f63918ed95b5a73e55f6225c4eaca30d78b87d74
dgarc359 <87497958+Dgarc359@users.noreply.github.com>
2025-02-09 11:51:29 -0500

    rename variables to line them up with what you would see in wikipedia, add some comments

--- scripts/rigid_car.gd
+++ scripts/rigid_car.gd
@@ -1,15 +1,6 @@
 extends RigidBody3D
 class_name RigidCar
 
-# current raycast position is (0, -0.2, 0) METERS
-
-
-# what units are these??
-#@export var suspension_full_length: float = 0.5
-#@export var spring_strength: float = 20.0
-#@export var spring_damper: float = 1.0
-#@export var wheel_radius: float = 0.33
-
 @onready var wheel_br: WheelController = $WheelBR
 @onready var wheel_fr: WheelController = $WheelFR
 @onready var wheel_bl: WheelController = $WheelBL
@@ -17,21 +8,23 @@ class_name RigidCar
 
 
 # reference for these vars: https://www.youtube.com/watch?v=6hEL66P1WQw
-@export var spring_distance_max: float = -0.2
-#@export var spring_constant: float = 42214.0 # ????? Reference is using magic numbers?????
-#@export var spring_damping: float = 7904.0 # ????? 
+@export var max_spring_length: float = 1.0
 
-@export var spring_constant: float = 40.0
-@export var spring_damping: float = 0.001
+@export var spring_stiffness: float = 1.0
+@export var spring_damping: float = 0.01
 
 
 
 func _ready():
-	var weight: float = mass * ProjectSettings.get_setting("physics/3d/default_gravity")
-	wheel_fl.init_suspension(weight / 4, spring_distance_max, spring_constant, spring_damping)
-	wheel_fr.init_suspension(weight / 4, spring_distance_max, spring_constant, spring_damping)
-	wheel_bl.init_suspension(weight / 4, spring_distance_max, spring_constant, spring_damping)
-	wheel_br.init_suspension(weight / 4, spring_distance_max, spring_constant, spring_damping)
+	# https://en.wikipedia.org/wiki/Weight#Gravitational_definition
+	var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
+	var weight: float = mass * gravity
+	print('gravity ', gravity)
+	print('mass ', mass)
+	wheel_fl.init_suspension(weight / 4, max_spring_length, spring_stiffness, spring_damping)
+	wheel_fr.init_suspension(weight / 4, max_spring_length, spring_stiffness, spring_damping)
+	wheel_bl.init_suspension(weight / 4, max_spring_length, spring_stiffness, spring_damping)
+	wheel_br.init_suspension(weight / 4, max_spring_length, spring_stiffness, spring_damping)
 
 func _physics_process(delta: float) -> void:
 	var vehicle_rotation = Quaternion(transform.basis)
--- scripts/wheel_ray_cast_2.gd
+++ scripts/wheel_ray_cast_2.gd
@@ -10,54 +10,69 @@ class_name WheelController
 # grab a reference to one, just refernce self as needed
 #@onready var ray : RayCast3D = get_node("RayCast3D")
 
-var spring_distance_max : float
-var spring_constant : float
+var max_spring_length : float
+var spring_stiffness : float
 var spring_damping : float
 var force: Vector3
 var offset : Vector3
-var spring_distance : float
-var spring_distance_now : float
+var previous_spring_compression : float
+var current_spring_compression : float
 var spring_rest_distance : float
 var wheel_position : Vector3
 var spring_velocity : float
 
 
-func init_suspension(rest_force: float, arg_spring_distance_max: float, arg_spring_constant: float, arg_spring_damping: float):
-	spring_distance_max = arg_spring_distance_max
-	spring_constant = arg_spring_constant
+func init_suspension(rest_force: float, arg_max_spring_length: float, arg_spring_stiffness: float, arg_spring_damping: float):
+	max_spring_length = arg_max_spring_length
+	spring_stiffness = arg_spring_stiffness
 	spring_damping = arg_spring_damping
-	target_position = Vector3(0, -(wheel_radius + spring_distance_max), 0)
-	spring_distance = 0
-	spring_rest_distance = rest_force / spring_constant
+	
+	# the wheel radius shouldn't give us a wheel bigger than our available spring
+	# otherwise, we get a negative target_position
+	# In reality, it's definitely possible to have a wheel radius > max spring length (monster trucks)
+	# But because we're deriving target_position for the raycast from wheel_radius
+	# we need to have some constraints... This seems like a spot for improvement
+	assert(wheel_radius <= max_spring_length) 
+	
+	target_position = Vector3(0, -(max_spring_length - wheel_radius), 0)
+	print('ray target position: ', target_position)
+	previous_spring_compression = 0
+	print('rest force ', rest_force)
+	print('spring stiffness ', spring_stiffness)
+	spring_rest_distance = rest_force / spring_stiffness
+	print('spring rest distance: ', spring_rest_distance)
 
 func apply_spring_force(delta: float, vehicle: RigidCar, vehicle_rotation: Quaternion):
 	if is_colliding():
 		var contact_point: Vector3 = get_collision_point()
 		var contact_point_vehicle: Vector3 = vehicle.to_local(contact_point)
-		spring_distance_now = contact_point_vehicle.y - wheel_radius
-		if spring_distance != 0:
-			spring_velocity = (spring_distance_now - spring_distance) / delta
+		current_spring_compression = contact_point_vehicle.y - wheel_radius
+		if previous_spring_compression != 0:
+			spring_velocity = (current_spring_compression - previous_spring_compression) / delta
 		else:
 			spring_velocity = 0
 		
-		var min_threshold = 0.1
-		if abs(spring_distance_now) < min_threshold and abs(spring_velocity) < min_threshold:
-			spring_distance_now = 0
-			spring_velocity = 0
+		previous_spring_compression = current_spring_compression
 		
-		spring_distance = spring_distance_now
+		# f = k * x (hooke's law) says that force = K which is some positive real number
+		# characteristic of a spring
+		# X should be how much our spring has compressed
+		# 							K ??			X
+		var spring_force: float = spring_stiffness * (spring_rest_distance - previous_spring_compression) # Hooke's law
 		
-		var spring_force: float = spring_constant * (spring_rest_distance - spring_distance) # Hooke's law
 		
 		var damping_force: float = spring_damping * spring_velocity
-		force = Vector3(0, spring_force + damping_force, 0)
+		var upward_force = spring_force + damping_force
+		print('upward force: ', upward_force)
+		force = Vector3(0, upward_force, 0)
 		#offset = vehicle_rotation * contact_point_vehicle
 		offset = contact_point - vehicle.global_position
 		vehicle.apply_force(force, offset)
-		wheel_position = Vector3(0, spring_distance, 0)
+		wheel_position = Vector3(0, previous_spring_compression, 0)
 	else:
-		spring_distance = 0
-		wheel_position = Vector3(0, -spring_distance_max, 0)
+		previous_spring_compression = 0
+		# TODO, sign on max_spring_length was flipped here
+		wheel_position = Vector3(0, max_spring_length, 0)
 	
 	# TODO
 	# wheel.transform.origin = wheel_position
